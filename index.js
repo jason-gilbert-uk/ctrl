89886e0e61b5188fea4482266773abe41fe65441
Jason Gilbert <jasongilbert@jasons-mac-mini.home>
2022-02-08 17:37:27 +0000

    latest

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const AWS = require('aws-sdk')
 
 
 async function writeConfig(dbTableName,region,config) {
-    console.log('writeConfig called')
     const docClient = new AWS.DynamoDB.DocumentClient({region});
 
     var item = {id: 'scrapingconfig', config: config}
     var params = {TableName: dbTableName, Item: item }
     try {
-        console.log('about to write')
         const result = await docClient.put(params).promise();
-        console.log('returned from write')
-        console.log(result);
         return result;      
     } catch (err) {
         console.log('error in writeConfig')
@@ -20,7 +16,6 @@ async function writeConfig(dbTableName,region,config) {
 }
 
 async function resetConfig(dbTableName,region) {
-    console.log('resetConfig called');
     var config = {
         urls : [{url:'https://www.tesco.com/groceries/en-GB/shop/fresh-food/all',state:"ready",nextInChain:""},
                 {url:'https://www.tesco.com/groceries/en-GB/shop/bakery/all',state:"ready",nextInChain:""},
@@ -36,9 +31,6 @@ async function resetConfig(dbTableName,region) {
             ]
         }
         var result = await writeConfig(dbTableName,region,config);
-        console.log('returned from writeConfig')
-        console.log('result was')
-        console.log(result);
         return result;
 }
 
